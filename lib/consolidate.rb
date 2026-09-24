910cd5f67756f52025e7f834c40da268931dbe7b
louismachin <louis@machin.dev>
2026-09-24 19:27:59 +0100

    Updated args

--- lib/consolidate.rb
+++ lib/consolidate.rb
@@ -1,11 +1,11 @@
-def consolidated_data(latitude, longitude, year, month, day, hour, minute, timezone_offset)
+def consolidated_data(latitude, longitude, year, month, day, hour, minute, timezone_offset = 0)
     return {
         :sidereal => sidereal_chart_values(latitude, longitude, year, month, day, hour, minute, timezone_offset),
         :tropical => tropical_chart_values(latitude, longitude, year, month, day, hour, minute, timezone_offset),
     }
 end
 
-def consolidated_data_and_charts(latitude, longitude, year, month, day, hour, minute, timezone_offset, uuid = nil, dir = './')
+def consolidated_data_and_charts(latitude, longitude, year, month, day, hour, minute, timezone_offset = 0, uuid = nil, dir = './')
     output_path = uuid ? dir + 'north_indian_chart_' + uuid + '.svg' : nil
     north_indian_chart(latitude, longitude, year, month, day, hour, minute, timezone_offset, :lahiri, output_path)
     output_path = uuid ? dir + 'western_chart_' + uuid + '.svg' : nil
